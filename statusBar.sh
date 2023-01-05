ccdbbc259d2641f80bcd8a075927585cb89c1833
Peter Maltby <peter.p.maltby@gmail.com>
2023-01-05 13:18:10 +0000

    status bar script for dwm

--- statusBar.sh
+++ statusBar.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+# statusBar.sh
+# author: peterm
+# created: 2023-01-05
+#############################################################
+source $S/PABLO.sh
+overunProtection=false
 
+pStart
 while true; do
-	xsetroot -name "`date`"
-	sleep 2
+	xsetroot -name "`date \"+%Y-%m-%d %T\"`"
+	sleep 1s
 done
+
+# should never run as x server will close down
+pEnd
