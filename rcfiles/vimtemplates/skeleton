92203fea72675de4ac8103d56fd31a73cf476aaf
Peter Maltby <mail@petermaltby.com>
2023-04-14 11:03:33 +0100

    added vim templates

---
+++ rcfiles/vimtemplates/skeleton
@@ -0,0 +1,5 @@
+[:EVAL:]expand("%:t")[:END:]
+author: [:EVAL:]$USER[:END:]
+created: [:EVAL:]strftime('%Y-%m-%d')[:END:]
+
+-------------------------------------------------------------
--- rcfiles/vimtemplates/skeleton.sh
+++ rcfiles/vimtemplates/skeleton.sh
@@ -2,4 +2,4 @@
 # [:EVAL:]expand("%:t")[:END:]
 # author: [:EVAL:]$USER[:END:]
 # created: [:EVAL:]strftime('%Y-%m-%d')[:END:]
-[:EVAL:]system("figlet -f small -k " . toupper(expand('%:t:r')) . " | sed 's/^/# /'")[:END:]#############################################################
+#############################################################
